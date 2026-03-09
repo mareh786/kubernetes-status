17cdc4a3a5f1a11d740ad36deefcb2200bb1586b
Ubuntu <ubuntu@master-1.us-central1-b.c.centering-crow-483408-n8.internal>
2026-03-09 05:27:28 +0000

    Added plain node check script

---
+++ node.sh
@@ -0,0 +1,25 @@
+i#!/bin/bash
+ 
+echo "Checking on: $(hostname)"
+ 
+#Availability of nodes
+ 
+NODES=$(kubectl get nodes --no-headers | wc -l)
+if [[ "$NODES" -eq 0 ]]; then
+        echo "NO NODES FOUND"
+        exit 1
+fi
+ 
+#Nodes Status Check
+ 
+NOT_READY_NODE=$(kubectl get node --no-headers | awk '$2 != "Ready" {print $1}' | wc -l)
+if [[ "$NOT_READY_NODE" -eq 0 ]]; then
+        echo "Nodes: Healthy"
+        echo "Total Nodes: $NODES"
+        exit 0
+else
+        echo "Nodes: Not Healthy"
+        echo "Unhealthy Nodes:"
+        kubectl get nodes --no-headers | awk '$2 != "Ready" {print $1}'
+        exit 1
+fi
